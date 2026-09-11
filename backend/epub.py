9eda17c50202f4f493aefaf850ebf31fc11aada5
Otávio Almeida <otaviochittolina@gmail.com>
2026-09-11 10:34:18 -0300

    fix: resolve unsupported emoji rendering on Kindle

--- backend/epub.py
+++ backend/epub.py
@@ -7,6 +7,7 @@ import uuid
 import zipfile
 from datetime import UTC, datetime
 from html import escape
+import re
 from pathlib import Path
 
 from .models import Article
@@ -24,30 +25,44 @@ table { border-collapse: collapse; max-width: 100%; } td, th { border: 1px solid
 math { font-size: 1.05em; } math[display="block"] { display: block; margin: 1em auto; text-align: center; }
 """.strip()
 
+EMOJI_RE = re.compile("[\\U0001F1E6-\\U0001F1FF\\U0001F300-\\U0001FAFF\\u2300-\\u23FF\\u2600-\\u27BF\\u2B00-\\u2BFF\\uFE0F\\u200D]")
+
+
+def without_emojis(value: str) -> str:
+    return EMOJI_RE.sub("", value)
+
 
 def epub_xhtml(article: Article) -> str:
+    title = without_emojis(article.title)
+    author = without_emojis(article.author)
+    source_url = without_emojis(article.source_url)
+    content_html = without_emojis(article.content_html)
     return f'''<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml"><head><title>{escape(article.title)}</title><link rel="stylesheet" type="text/css" href="style.css"/></head>
-<body><article><h1 class="title">{escape(article.title)}</h1><p class="byline">{escape(article.author)}</p>{article.content_html}<p class="source">Source: <a href="{escape(article.source_url, quote=True)}">{escape(article.source_url)}</a></p></article></body></html>'''
+<html xmlns="http://www.w3.org/1999/xhtml"><head><title>{escape(title)}</title><link rel="stylesheet" type="text/css" href="style.css"/></head>
+<body><article><h1 class="title">{escape(title)}</h1><p class="byline">{escape(author)}</p>{content_html}<p class="source">Source: <a href="{escape(source_url, quote=True)}">{escape(source_url)}</a></p></article></body></html>'''
 
 
 def nav_xhtml(article: Article) -> str:
-    items = "".join(f'<li><a href="article.xhtml#{escape(item_id)}">{escape(text)}</a></li>' for item_id, text in article.headings)
+    items = "".join(f'<li><a href="article.xhtml#{escape(item_id)}">{escape(without_emojis(text))}</a></li>' for item_id, text in article.headings)
+    title = without_emojis(article.title)
     return f'''<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml"><head><title>Contents</title></head><body><nav epub:type="toc" id="toc" xmlns:epub="http://www.idpf.org/2007/ops"><h1>Contents</h1><ol><li><a href="article.xhtml">{escape(article.title)}</a></li>{items}</ol></nav></body></html>'''
+<html xmlns="http://www.w3.org/1999/xhtml"><head><title>Contents</title></head><body><nav epub:type="toc" id="toc" xmlns:epub="http://www.idpf.org/2007/ops"><h1>Contents</h1><ol><li><a href="article.xhtml">{escape(title)}</a></li>{items}</ol></nav></body></html>'''
 
 
 def write_epub(article: Article, destination: Path) -> None:
     identifier = uuid.uuid5(uuid.NAMESPACE_URL, article.source_url)
+    title = without_emojis(article.title)
+    author = without_emojis(article.author)
+    source_url = without_emojis(article.source_url)
     image_manifest = "".join(
         f'<item id="image-{index}" href="{escape(asset.href, quote=True)}" media-type="{escape(asset.media_type, quote=True)}"/>'
         for index, asset in enumerate(article.images, start=1)
     )
     manifest = '<item id="article" href="article.xhtml" media-type="application/xhtml+xml"/><item id="nav" href="nav.xhtml" media-type="application/xhtml+xml" properties="nav"/><item id="css" href="style.css" media-type="text/css"/>' + image_manifest
     opf = f'''<?xml version="1.0" encoding="utf-8"?>
-<package xmlns="http://www.idpf.org/2007/opf" unique-identifier="book-id" version="3.0"><metadata xmlns:dc="http://purl.org/dc/elements/1.1/"><dc:identifier id="book-id">urn:uuid:{identifier}</dc:identifier><dc:title>{escape(article.title)}</dc:title><dc:creator>{escape(article.author)}</dc:creator><dc:language>en</dc:language><dc:source>{escape(article.source_url)}</dc:source><meta property="dcterms:modified">{datetime.now(UTC).strftime('%Y-%m-%dT%H:%M:%SZ')}</meta></metadata><manifest>{manifest}</manifest><spine><itemref idref="article"/></spine></package>'''
+<package xmlns="http://www.idpf.org/2007/opf" unique-identifier="book-id" version="3.0"><metadata xmlns:dc="http://purl.org/dc/elements/1.1/"><dc:identifier id="book-id">urn:uuid:{identifier}</dc:identifier><dc:title>{escape(title)}</dc:title><dc:creator>{escape(author)}</dc:creator><dc:language>en</dc:language><dc:source>{escape(source_url)}</dc:source><meta property="dcterms:modified">{datetime.now(UTC).strftime('%Y-%m-%dT%H:%M:%SZ')}</meta></metadata><manifest>{manifest}</manifest><spine><itemref idref="article"/></spine></package>'''
     container = '''<?xml version="1.0" encoding="UTF-8"?><container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container"><rootfiles><rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/></rootfiles></container>'''
     temporary = None
     try:
